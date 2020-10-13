0c57e19a6cbca72edcc18c0b0566177e6d2a787c
guopeng li <guopeng.li@technicolor.com>
2020-10-13 11:06:19 +0200

    implement toolbar back home

--- app/src/main/java/com/lig/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/lig/happyplaces/AddHappyPlaceActivity.kt
@@ -2,10 +2,17 @@ package com.lig.happyplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_happy_place.*
 
 class AddHappyPlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
+        setSupportActionBar(toolbar_add_place)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar_add_place.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
+
 }
