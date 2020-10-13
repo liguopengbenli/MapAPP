008e74df52109032438b6dc1840580486333b54a
guopeng li <guopeng.li@technicolor.com>
2020-10-13 15:04:56 +0200

    Using DEXTER for easy set permission for application + custom answer with alertDialog

--- app/src/main/java/com/lig/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/lig/happyplaces/AddHappyPlaceActivity.kt
@@ -1,12 +1,25 @@
 package com.lig.happyplaces
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.DatePickerDialog
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kotlinx.android.synthetic.main.activity_add_happy_place.*
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -22,7 +35,7 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
         toolbar_add_place.setNavigationOnClickListener {
             onBackPressed()
         }
-        dateSetListener = DatePickerDialog.OnDateSetListener{view, year, month, dayOfMonth ->
+        dateSetListener = DatePickerDialog.OnDateSetListener{ view, year, month, dayOfMonth ->
             cal.set(Calendar.YEAR, year)
             cal.set(Calendar.MONTH, month)
             cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
@@ -30,16 +43,73 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
         }
 
         et_date.setOnClickListener(this)
+        tv_add_image.setOnClickListener(this)
     }
 
     // when there are lots of views for onlick using this structure to simplifier
     override fun onClick(v: View?) {
         when(v!!.id){
-            R.id.et_date ->{
-                DatePickerDialog(this@AddHappyPlaceActivity,
-                    dateSetListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
+            R.id.et_date -> {
+                DatePickerDialog(
+                    this@AddHappyPlaceActivity,
+                    dateSetListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(
+                        Calendar.DAY_OF_MONTH
+                    )
+                ).show()
+            }
+            R.id.tv_add_image -> {
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogItems = arrayOf("Select photo from Gallery", "Capture photo from camera")
+                pictureDialog.setItems(pictureDialogItems) { dialog, which ->
+                    when (which) {
+                        0 -> choosePhotoFromGallery()
+                        1 -> Toast.makeText(this@AddHappyPlaceActivity, "Camera selection coming soon...", Toast.LENGTH_SHORT).show()
+                    }
+                }.show()
             }
         }
+    }
+
+    private fun choosePhotoFromGallery(){
+        Dexter.withContext(this)
+            .withPermissions(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport) {
+                    if(report.areAllPermissionsGranted()){
+                        Toast.makeText(this@AddHappyPlaceActivity, "Storage read/write permission are granted", Toast.LENGTH_SHORT)
+                    }
+                }
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>,
+                    token: PermissionToken
+                ) {
+                   // token.continuePermissionRequest(); The most simple way to ask permission again
+                    showRationalDialogForPermission() //custom implementation
+                }
+            }).onSameThread().check() //
+    }
+
+
+    private fun showRationalDialogForPermission(){
+        AlertDialog.Builder(this)
+            .setMessage("It looks like you have turned off permission required for this feature. It can be enable under Application settings")
+            .setPositiveButton("GO TO SETTINGS")
+            { _,_->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                }catch (e: ActivityNotFoundException){
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel"){ dialog, _->
+                dialog.dismiss()
+            }.show()
 
     }
 
