db1791b9926889b035ec4bdfba5e70bb921f4244
guopeng li <guopeng.li@technicolor.com>
2020-10-13 13:39:25 +0200

    Adding a calendar picker

--- app/src/main/java/com/lig/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/lig/happyplaces/AddHappyPlaceActivity.kt
@@ -1,10 +1,19 @@
 package com.lig.happyplaces
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_add_happy_place.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
@@ -13,6 +22,32 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         toolbar_add_place.setNavigationOnClickListener {
             onBackPressed()
         }
+        dateSetListener = DatePickerDialog.OnDateSetListener{view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateIntent()
+        }
+
+        et_date.setOnClickListener(this)
     }
+
+    // when there are lots of views for onlick using this structure to simplifier
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.et_date ->{
+                DatePickerDialog(this@AddHappyPlaceActivity,
+                    dateSetListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+        }
+
+    }
+
+    private fun updateDateIntent(){
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        et_date.setText(sdf.format(cal.time).toString())
+    }
+
 
 }
