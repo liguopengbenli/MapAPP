a6875c657cf6796340b9156feede780137438fcc
guopeng li <guopeng.li@technicolor.com>
2020-10-14 11:17:35 +0200

    using lib for circle img + add recycle adapter + populate recyclerview

--- app/src/main/java/com/lig/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/lig/happyplaces/activities/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.lig.happyplaces.R
+import com.lig.happyplaces.adapters.HappyPlacesAdapter
 import com.lig.happyplaces.database.DatabaseHandler
 import com.lig.happyplaces.models.HappyPlaceModel
 import kotlinx.android.synthetic.main.activity_main.*
@@ -25,9 +28,21 @@ class MainActivity : AppCompatActivity() {
         val getHappyPlaceList: ArrayList<HappyPlaceModel> = dbHandler.getHappyPlaceList()
 
         if(getHappyPlaceList.size>0){
-            for(i in getHappyPlaceList){
-                Log.e("Title : ", i.title)
-            }
+           rv_happy_places_list.visibility = View.VISIBLE
+            tv_no_records_available.visibility = View.GONE
+            setupHappyPlacesRecyclerView(getHappyPlaceList)
+        }else{
+            rv_happy_places_list.visibility = View.GONE
+            tv_no_records_available.visibility = View.VISIBLE
         }
     }
+
+    private fun setupHappyPlacesRecyclerView(happyPlaceList: ArrayList<HappyPlaceModel>){
+        rv_happy_places_list.layoutManager = LinearLayoutManager(this)
+        rv_happy_places_list.setHasFixedSize(true)
+        val placesAdapter = HappyPlacesAdapter(this, happyPlaceList)
+        rv_happy_places_list.adapter = placesAdapter
+    }
+
+
 }
