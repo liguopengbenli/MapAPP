35bb2322efdbfa48d644a7c2235d5424311b6946
guopeng li <guopeng.li@technicolor.com>
2020-10-15 12:11:39 +0200

    display the location with a marker and zoom in

--- app/src/main/java/com/lig/happyplaces/activities/MapActivity.kt
+++ app/src/main/java/com/lig/happyplaces/activities/MapActivity.kt
@@ -2,17 +2,48 @@ package com.lig.happyplaces.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.lig.happyplaces.R
+import com.lig.happyplaces.models.HappyPlaceModel
+import kotlinx.android.synthetic.main.activity_map.*
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private var mHappyPlaceDetails: HappyPlaceModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
+        if(intent.hasExtra(MainActivity.EXTRA_PLACE_DETAILS)){
+            mHappyPlaceDetails = intent.getParcelableExtra(MainActivity.EXTRA_PLACE_DETAILS) as HappyPlaceModel
+        }
 
+        if(mHappyPlaceDetails != null){
+            setSupportActionBar(toolbar_happy_map)
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.title = mHappyPlaceDetails!!.title
+
+            toolbar_happy_map.setNavigationOnClickListener {
+                onBackPressed()
+            }
+        }
+
+        val supportMapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        supportMapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val position = LatLng(mHappyPlaceDetails!!.latitude, mHappyPlaceDetails!!.longitude)
+        googleMap!!.addMarker(MarkerOptions().position(position).title(mHappyPlaceDetails!!.location)) //add marker on the position plus display title
+        val newLatLngZoom = CameraUpdateFactory.newLatLngZoom(position, 15f) // zoom in
+        googleMap.animateCamera(newLatLngZoom)
     }
 
 
